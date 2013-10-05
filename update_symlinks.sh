1a592cc5bf04774f588923707311850666c31eee
ramon quezada <rpq@winscores.com>
2013-10-05 16:21:01 -0400

    make them symlinks

---
+++ update_symlinks.sh
@@ -0,0 +1,13 @@
+ln -fs ~/dotfiles/.bash_profile ~/.bash_profile
+ln -fs ~/dotfiles/.bashrc ~/.bashrc
+ln -fs ~/dotfiles/.bash_exports ~/.bash_exports
+ln -fs ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -fs ~/dotfiles/.bash_additional ~/.bash_additional
+
+ln -fs ~/dotfiles/.vimrc ~/.vimrc
+
+ln -fs ~/dotfiles/.git-bash-completion ~/.git-bash-completion
+ln -fs ~/dotfiles/.gitconfig ~/.gitconfig
+ln -fs ~/dotfiles/.gitignore ~/.gitignore
+
+ln -fs ~/dotfiles/.screenrc ~/.screenrc
